805c0b9f6914aca398e1212a447c86388f8f6272
MilanRottiers <milan.rottiers@gmail.com>
2024-11-28 12:24:06 +0100

    added beginning of shooting mechanic

---
+++ GameDevProject/GameDevProject/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject
+{
+    internal class Bullet
+    {
+        private Texture2D bulletSprite;
+        private Vector2 pos;
+        private Vector2 direction;
+        private float speed = 5f;
+
+        public Bullet(Texture2D bulletSprite, Vector2 startPos, Vector2 direction)
+        {
+            this.bulletSprite = bulletSprite;
+            this.pos = startPos;
+            this.direction = Vector2.Normalize(direction);
+        }
+
+        public void Update()
+        {
+            pos += direction * speed;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(bulletSprite, pos, Color.Red);
+        }
+    }
+
+}
---
+++ GameDevProject/GameDevProject/PlayerShoot.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject
+{
+    internal class PlayerShoot
+    {
+        Texture2D bulletSprite;
+        Vector2 pos;
+
+        private List<Bullet> bullets = new List<Bullet>();
+
+        float counter = 0;
+
+        public PlayerShoot(Texture2D bulletSprite, Vector2 pos)
+        {
+            this.bulletSprite = bulletSprite;
+            this.pos = pos;
+        }
+
+        public void Update()
+        {
+            counter++;
+            if (counter > 29)
+            {
+                counter = 0;
+            }
+
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                Vector2 direction = Mouse.GetState().Position.ToVector2() - pos;
+                bullets.Add(new Bullet(bulletSprite, this.pos, direction));
+            }
+
+
+            // Update bullets
+            foreach (var bullet in bullets)
+            {
+                bullet.Update();
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var bullet in bullets)
+            {
+                bullet.Draw(spriteBatch);
+            }
+        }
+
+    }
+}
